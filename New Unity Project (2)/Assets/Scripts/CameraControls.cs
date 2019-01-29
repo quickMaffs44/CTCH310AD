7ea3138962e264c9eb1e26516adef3e23d0cb326
quickMaffsCL135 <labios.earljustin@yahoo.com>
2019-01-29 11:21:59 -0600

    Camera controls and obligatory helloworld
    Added obligatory hello world script
    
    Added fwrd/bck and L/R movement to camera

---
+++ New Unity Project (2)/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+
+    public float speed;
+    Vector3 position;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.x -= speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            position.x += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.z += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            position.z -= speed;
+            this.transform.position = position;
+        }
+    }
+}
---
+++ New Unity Project (2)/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    private float offsetx;
+    private float offsety;
+    private float offsetz;
+    Vector3 position;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+        //offsetx = transform.position.x - player.transform.position.x;
+        //offsety = transform.position.z - player.transform.position.z;
+        //position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    position.x--;
+        //    this.transform.position = position;
+        //}
+
+    }
+
+    // called after Update every frame
+    private void LateUpdate() 
+    {
+        transform.position = player.transform.position + offset;
+
+        //position = new Vector3(player.transform.position.x - offsetx,
+        //                       transform.position.y - offsety,
+        //                       player.transform.position.z - offsetz);
+        //this.transform.position = position;
+    }
+}
