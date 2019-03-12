d7a18c01d0c746fff1a1802b3b31026440195534
quickMaffsCL135 <labios.earljustin@yahoo.com>
2019-03-12 11:14:27 -0600

    Prefabs

--- New Unity Project (2)/Assets/Scripts/Player.cs
+++ New Unity Project (2)/Assets/Scripts/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject bullet;
+    public Transform bulletSpawn;
+    public float bulletSpeed;
+    public float fallSpeed;
+    public float bulletLifetime;
+
+
+    public float rotateSpeed;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // another way to move the player ______________
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        //________________
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Create bullet from the prefab
+        // at the spawn point position and rotation
+        var newBullet = (GameObject)Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+
+        // add velocity to bullet
+        var bulletRB = newBullet.GetComponent<Rigidbody>();
+        bulletRB.velocity = newBullet.transform.forward * bulletSpeed;
+
+        // Destroy bullet after 2 seconds
+        Destroy(newBullet, bulletLifetime);
+
+
     }
 }
