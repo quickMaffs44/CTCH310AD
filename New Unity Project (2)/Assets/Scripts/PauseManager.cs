587a72c2067b9745df15e00e4164c4d7008747e7
quickMaffsCL135 <labios.earljustin@yahoo.com>
2019-04-02 11:02:48 -0600

    UI canvas and panel
    Made a pause screen

---
+++ New Unity Project (2)/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField]
+    Transform UIPanel;
+
+    bool isPaused;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIPanel.gameObject.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        UIPanel.gameObject.SetActive(true);
+    }
+    public void Unpause()
+    {
+        isPaused = false;
+        UIPanel.gameObject.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
